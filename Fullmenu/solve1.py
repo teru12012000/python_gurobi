93b635ac5b9f829923bcea15b597d4ac60c0615a
teru12012000 <sion0012@outlook.jp>
2022-10-22 19:46:24 +0900

    add fullmenu

---
+++ Fullmenu/solve1.py
@@ -0,0 +1,66 @@
+#jogとペース走とOFF(14日間)
+import math
+import gurobipy as gp
+model=gp.Model('jog_or_pacerun')
+n=14
+
+x1=[]
+x2=[]
+x3=[]
+x4=[]
+x5=[]
+x6=[]
+x7=[]
+A7=list(range(11))
+for i in range(n):
+ x1.append(model.addVar(vtype="B"))
+ x2.append(model.addVar(vtype="B"))
+ x3.append(model.addVar())
+ x4.append(model.addVar(vtype="B"))
+ x5.append(model.addVar(vtype="B"))
+ x6.append(model.addVar(vtype="B"))
+ x7.append(model.addVar(vtype="B"))
+Z=model.addVar()
+model.update()
+
+fig=0
+fit=0
+for i in range(n):
+  model.addConstr(x1[i]+x2[i]+x4[i]+x5[i]+x6[i]+x7[i]<=1)
+  model.addConstr(x3[i]<=20)
+  model.addConstr(x3[i]>=6)
+  
+for i in range((n//2)+1):
+  fig*=math.exp(-1/15)
+  fig+=2*(x3[i]*(1.1*x1[i]+1.65*x2[i])+35*x4[i]+40*x5[i]+40*x6[i]+34*x7[i])
+  fit*=math.exp(-1/45)
+  fit+=(x3[i]*(1.1*x1[i]+1.65*x2[i])+35*x4[i]+40*x5[i]+40*x6[i]+34*x7[i])
+  model.addConstr(fit-fig>=(-1*(i+1)*20))
+for i in range((n//2)+1,n):
+  fig*=math.exp(-1/15)
+  fig+=2*(x3[i]*(1.1*x1[i]+1.65*x2[i])+35*x4[i]+40*x5[i]+40*x6[i]+34*x7[i])
+  fit*=math.exp(-1/10)
+  fit+=(x3[i]*(1.1*x1[i]+1.65*x2[i])+35*x4[i]+40*x5[i]+40*x6[i]+34*x7[i])
+  model.addConstr(fit-fig>=(-1*(i+1)*10))
+model.addConstr(gp.quicksum(x1[i] for i in range(n))+gp.quicksum(x2[i] for i in range(n))>=6)
+model.addConstr(gp.quicksum(x4[i] for i in range(n))+gp.quicksum(x5[i] for i in range(n))==2)
+model.addConstr(gp.quicksum(x6[i] for i in range(n))==1)
+model.addConstr(gp.quicksum(x7[i] for i in range(n))==1)  
+model.setObjective(gp.quicksum((x3[i-1]*(1.1*x1[i-1]+1.65*x2[i-1])+35*x4[i-1]+40*x5[i-1]+40*x6[i-1]+34*x7[i-1])*math.exp(-(n-i)/45) for i in range(1,n+1)),gp.GRB.MAXIMIZE)
+model.optimize()
+print(model.ObjVal)
+fig=0
+fit=0
+for i in range(n):
+  print(x1[i].X,x2[i].X,x3[i].X,x4[i].X,x5[i].X,x6[i].X,x7[i].X)
+  fig*=math.exp(-1/15)
+  fig+=2*(x3[i].X*(1.1*x1[i].X+1.65*x2[i].X)+35*x4[i].X+40*x5[i].X+40*x6[i].X+34*x7[i].X)
+  fit*=math.exp(-1/45)
+  fit+=(x3[i].X*(1.1*x1[i].X+1.65*x2[i].X)+35*x4[i].X+40*x5[i].X+40*x6[i].X+34*x7[i].X)
+print(fit-fig)
+  
+  
+
+    
+
+
